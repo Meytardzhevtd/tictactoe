c43358cba0e8ceb66ec4d5e0db0a6ce3eaf1fc64
Meytardzhevtd <meytardzhevtd@yandex.ru>
2026-01-28 23:33:27 +0300

    tictac

--- src/main/java/com/example/tictactoe/Controller/AuthController.java
+++ src/main/java/com/example/tictactoe/Controller/AuthController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/api")
@@ -15,12 +17,21 @@ public class AuthController {
     private final AtomicLong userIdGenerator = new AtomicLong(1);
 
     @PostMapping("/register")
-    public String registPlayer(@RequestParam String name, @RequestParam String email) {
-        Long userId = userIdGenerator.getAndIncrement();
-        Player player = new Player(userId, name, email);
-        users.put(userId, player);
-        System.out.println("User registred: " + player);
-        return "redirect:/profile.html";
+    public String registPlayer(@RequestParam String name, @RequestParam String email, @RequestParam String password) {
+        System.out.println(
+                "Пользователь зарегистрирован. Name = " + name + " Email = " + email + " Password = " + password);
+
+        return "redirect:/login.html";
     }
+
+    // @PostMapping("/passwordVerificatin")
+    // public String changePassword(@RequestParam Long userId, @RequestParam String
+    // oldPassword,
+    // @RequestParam String newPassword) {
+    // Player player = users.get(userId);
+    // if (player == null) {
+    // return "redirect:/changepassword.html?error=invalid_user";
+    // }
+    // }
 
 }
